33bc163949d827d5d11d076216fea59dc1caf879
mamogmx <marco.carbone@gmx.com>
2015-10-04 08:38:11 +0200

    first

---
+++ sue-web/index.php
@@ -0,0 +1,25 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+<html ng-app="phonecatApp">
+<head>
+    <script src="../js/angular.js"></script>
+  <script src="js/controllers.js"></script>
+</head>
+<body ng-controller="PhoneListCtrl">
+
+  <ul>
+    <li ng-repeat="phone in phones">
+      <span>{{phone.name}}</span>
+      <p>{{phone.snippet}}</p>
+    </li>
+  </ul>
+
+</body>
+</html>
